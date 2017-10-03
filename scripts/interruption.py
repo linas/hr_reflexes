11cbeea977df9e0be1c3643960ce19a255066a4d
RalfMayet <trilly@gmail.com>
2017-10-03 20:16:48 +0200

    random apologies

--- scripts/interruption.py
+++ scripts/interruption.py
@@ -5,22 +5,22 @@ Using chin movement to determine that user is still talking
 https://github.com/hansonrobotics/hr_reflexes/issues/6
 
 Conditions:
-  - [ ] Only trigger if there is exactly one face
-  - [ ] Moving Window over /realsense face.mouth_open over threshold
-  - [ ] If Sophia is speaking (/sophia6/speech_events listen to start/stop)
-  - [ ] STT event was under threshold of time away (last event on /sophia6/perceived_text)
+  - Only trigger if there is exactly one face
+  - Moving Window over /realsense face.mouth_open over threshold
+  - If Sophia is speaking (/sophia6/speech_events listen to start/stop)
+  - STT event was under threshold of time away (last event on /sophia6/perceived_text)
 
 Trigger:
-  - [ ] Shut up (str data "shutup" on /sophia6/tts_control)
-  - [ ] Pop from stack of >10 apologies and say them (str data ".." on /sophia6/tts)
-  - [ ] Backtrack in Dialog (DOC: not possible in current release, Wenwei must update Chatscript server for this to work. Will work via :retry or :redo command.)
-  - [ ] WORKAROUND: Interrupt Sophia's speech and repeat last utterance. 
+  - Shut up (str data "shutup" on /sophia6/tts_control)
+  - Pop from stack of >10 apologies and say them (str data ".." on /sophia6/tts)
+  - [ ] Backtrack in Dialog (not possible in current release, Wenwei must update Chatscript server for this to work. Will work via :retry or :redo command.)
 """
 
 import rospy
 import numpy as np
 from time import sleep
 import time, threading
+import random
 
 from realsense_ros.msg import *
 from pau2motors.msg import pau
@@ -38,6 +38,14 @@ class realsense_stream:
         self.sophiaSpeaking = False
         self.userSpokeRecently = False
 
+        self.apologies = ["Oh sorry, what?",
+                          "What was that?",
+                          "Oh you were saying?",
+                          "Excuse me, can you repeat?",
+                          "Can you repeat that?",
+                          "Oh what?",
+                          "Mhh?"]
+
         rospy.init_node('interrupt_reflex')
 
         # Subscribers
@@ -50,19 +58,24 @@ class realsense_stream:
         self.publishSpeechEvent = rospy.Publisher('/sophia6/tts', String, queue_size=1)
 
         # Debug output
-        self.debugOutput()
+        self.mainLoop()
 
 
 
-    def debugOutput(self):
+    def mainLoop(self):
       print("activeFaces: %d, mouthOpen: %f, sophia speaking: %d, user spoke recently: %d" % (self.activeFaces, self.avgMouthOpen, self.sophiaSpeaking, self.userSpokeRecently))
 
-      if self.activeFaces == 1 and self.avgMouthOpen >= self.avgMouthOpenThreshold and self.sophiaSpeaking and self.userSpokeRecently:
-        print("TRIGGERED!")
-        self.publishShutup.publish(String("shutup"))
-        self.publishSpeechEvent.publish(String("Oh I'm sorry, did not mean to interrupt"))
+      if self.activeFaces == 1 
+         and self.avgMouthOpen >= self.avgMouthOpenThreshold 
+         and self.sophiaSpeaking 
+         and self.userSpokeRecently:
 
-        sleep(5)
+        print("Interruption detected!")
+        self.publishShutup.publish(String("shutup"))
+        self.publishSpeechEvent.publish(String(random.choice(self.apologies)))
+
+        # refractory period
+        sleep(self.userSpokeThreshold + 2)
 
       if not rospy.is_shutdown():
         threading.Timer(0.5, self.debugOutput).start()
@@ -81,7 +94,6 @@ class realsense_stream:
       self.userSpokeRecently = True
       threading.Timer(self.userSpokeThreshold, self.resetLastSpeechInput).start()
 
-
     def parseRealsense(self, msg):
         self.activeFaces = len(msg.faces)
 
@@ -97,7 +109,6 @@ class realsense_stream:
         else:
           self.avgMouthOpenStack = [0 for _ in range(self.avgMouthOpenWindowSize)]
 
-          
 
 if __name__ == "__main__":
     stream = realsense_stream()
