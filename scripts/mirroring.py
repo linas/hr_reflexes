a828113f45465e16c9d99e36e3dc00d6fb70ae62
Wenwei Huang <dut.hww@gmail.com>
2017-11-13 12:02:13 +0800

    moving average mirroring

--- scripts/mirroring.py
+++ scripts/mirroring.py
@@ -12,6 +12,20 @@ class realsense_stream:
         self.decibelLimit = 40.0
         self.decibelCurrent = 0.0
 
+        # percentage of sensor value that gets added each step.
+        self.browDelay = 0.25
+        self.mouthDelay = 0.0 # deactivate mouth mirroring...
+        self.blinkDelay = 0.3
+
+        #
+        self.leftBrowRaise = 0.0
+        self.leftBrowLower = 0.0
+        self.rightBrowRaise = 0.0
+        self.rightBrowLower = 0.0
+        self.mouthOpen = 0.0
+        self.eyesClosed = 0.0
+
+
         rospy.init_node('realsense')
 
         # Subscribers
@@ -28,6 +42,7 @@ class realsense_stream:
         sleep(1.5)
         self.animationmodePub.publish(self.animationmode)
 
+
     def parseAudioSensor(self, msg):
       self.decibelCurrent = msg.Decibel
 
@@ -35,26 +50,31 @@ class realsense_stream:
         if len(msg.faces) == 1:
           face = msg.faces[0]
 
-          if self.decibelCurrent >= self.decibelLimit:
-            mouthopen = 0.0
-          else:
-            mouthopen = float(face.mouth_open) / 100.0
+          #if self.decibelCurrent >= self.decibelLimit:
+          #  mouthopen = 0.0
+          #else:
+          #  mouthopen = float(face.mouth_open) / 100.0
 
-          left_brow_raise = float(face.left_brow_raise) / 100.0
-          left_brow_lower = float(face.left_brow_lower) / 100.0
-          right_brow_raise = float(face.right_brow_raise) / 100.0
-          right_brow_lower = float(face.right_brow_lower) / 100.0
+          self.leftBrowRaise =  ((1.0 - self.browDelay) * self.leftBrowRaise) + (self.browDelay * (float(face.left_brow_raise) / 100.0))
+          self.leftBrowLower =  ((1.0 - self.browDelay) * self.leftBrowLower) + (self.browDelay * (float(face.left_brow_lower) / 100.0))
+          self.rightBrowRaise = ((1.0 - self.browDelay) * self.rightBrowRaise) + (self.browDelay * (float(face.right_brow_raise) / 100.0))
+          self.rightBrowLower = ((1.0 - self.browDelay) * self.rightBrowLower) + (self.browDelay * (float(face.right_brow_lower) / 100.0))
 
-          eye_closed_signal = float(face.left_eye_closed + face.right_eye_closed) / 200.0
+          self.mouthOpen = ((1.0 - self.mouthDelay) * self.mouthOpen) + (self.mouthDelay * (float(face.mouth_open) / 100.0))
+          self.eyesClosed = ((1.0 - self.blinkDelay) * self.eyesClosed) + (self.blinkDelay * (float(face.left_eye_closed + face.right_eye_closed) / 200.0))
+
+          
 
           msg = pau()
-          msg.m_coeffs = [mouthopen, left_brow_raise, left_brow_raise/2.0, left_brow_lower, 
-                          right_brow_raise, right_brow_raise/2.0, right_brow_lower,
-                          eye_closed_signal, eye_closed_signal, eye_closed_signal, eye_closed_signal]
+          msg.m_coeffs = [self.mouthOpen, self.leftBrowRaise, self.leftBrowRaise/1.5, self.leftBrowLower, 
+                          self.rightBrowRaise, self.rightBrowRaise/1.5, self.rightBrowLower,
+                          self.eyesClosed, self.eyesClosed, self.eyesClosed, self.eyesClosed]
 
           msg.m_shapekeys = ['lip-JAW.DN', 'brow_outer_UP.L', 'brow_inner_UP.L', 'brow_outer_DN.L', 
                             'brow_outer_up.R', 'brow_inner_UP.R', 'brow_outer_DN.R',
                              'eye-blink.UP.R', 'eye-blink.UP.L', 'eye-blink.LO.R', 'eye-blink.LO.L']
+
+          print(msg.m_coeffs)
 
           self.animationmodePub.publish(self.animationmode)
           self.pub.publish(msg)
